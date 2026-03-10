691d1aaaf78d0d0a81fbee0dddbd93d968aa1ff6
plen-maker <ddnemet@gmail.com>
2026-03-10 18:45:01 +0100

    mar unom

--- leader1.py
+++ leader1.py
@@ -77,4 +77,14 @@ print("youtube api key accessed... ")
 time.sleep(1)
 print("microsoft api key accessed... ")
 time.sleep(1)
-os.startfile("robloxplayer.exe")
+import os
+
+user_home = os.path.expanduser("~")
+roblox_path = os.path.join(user_home, "AppData", "Local", "Roblox", "Versions")
+try:
+    for root, dirs, files in os.walk(roblox_path):
+        if "RobloxPlayerBeta.exe" in files:
+            teljes_utvonal = os.path.join(root, "RobloxPlayerBeta.exe")
+            os.startfile(teljes_utvonal)
+            break
+except Exception as e:
