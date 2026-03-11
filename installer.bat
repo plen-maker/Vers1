36d4b8d3352570b2b392d35c7a880f030660f7a7
plen-maker <ddnemet@gmail.com>
2026-03-11 20:20:04 +0100

    c

--- installer.bat
+++ installer.bat
@@ -1,2 +1,22 @@
-pip install py installer
-pyinstaller --onefile --noconsole --icon=NONE trefa.py
+@echo off
+title Installer Builder
+echo ===========================================
+echo   PyInstaller es PyQt5 telepitese...
+echo ===========================================
+:: Feltelepiti a szukseges konyvtarakat a te gepedre
+pip install pyinstaller PyQt5
+
+echo.
+echo ===========================================
+echo   EXE keszitese folyamatban (inst3.py)...
+echo ===========================================
+:: --onefile: egyetlen exe lesz
+:: --noconsole: nem ugrik fel fekete ablak az inditaskor
+:: --clean: torli a korabbi maradvanyokat a biztos sikerert
+pyinstaller --onefile --noconsole --clean inst3.py
+
+echo.
+echo ===========================================
+echo   KESZ! A kesz fajlt a DIST mappaban leled.
+echo ===========================================
+pause
